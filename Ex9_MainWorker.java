3defe6ac56ace23c9206ca8c11d2a94c0a2abdbb
NguyenDuyBao_20066051 <100182329+nguyenduybao1@users.noreply.github.com>
2023-06-23 14:31:29 +0700

    Update Ex9_MainWorker.java

--- Ex9_MainWorker.java
+++ Ex9_MainWorker.java
@@ -2,18 +2,18 @@ package Ex9;
 
 public class Ex9_MainWorker {
 	public static void main(String[] args) {
-		Ex9_Worker congNhan1 = new Ex9_Worker(1, "Nguyen", "Thinh", 100);
-		Ex9_Worker congNhan2 = new Ex9_Worker(2, "Tran", "Thuy", 300);
-		Ex9_Worker congNhan3 = new Ex9_Worker(3, "Nguyen", "Bao", 500);
-		Ex9_Worker congNhan4 = new Ex9_Worker(4, "Nguyen", "Dien", 700);
+		Ex9_Worker worker1 = new Ex9_Worker(1, "Nguyen", "Bao", 100);
+		Ex9_Worker worker2 = new Ex9_Worker(2, "Tang", "Ngan", 300);
+		Ex9_Worker worker3 = new Ex9_Worker(3, "Nguyen", "Thinh", 500);
+		Ex9_Worker worker4 = new Ex9_Worker(4, "Chau", "Tan", 700);
 		
-		System.out.println(congNhan1);
-		System.out.println(congNhan1.tinhLuong());
-		System.out.println(congNhan2);
-		System.out.println(congNhan2.tinhLuong());
-		System.out.println(congNhan3);
-		System.out.println(congNhan3.tinhLuong());
-		System.out.println(congNhan4);
-		System.out.println(congNhan4.tinhLuong());
+		System.out.println(worker1);
+		System.out.println(worker1.calSalary());
+		System.out.println(worker2);
+		System.out.println(worker2.calSalary());
+		System.out.println(worker3);
+		System.out.println(worker3.calSalary());
+		System.out.println(worker4);
+		System.out.println(worker4.calSalary());
 	}
 }
